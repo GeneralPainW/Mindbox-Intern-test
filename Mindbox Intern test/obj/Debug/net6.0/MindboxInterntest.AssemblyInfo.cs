059db9e2186c60363eda1c95348db91d0cf7f66c
Alexandr <sasha.spiridonov@bk.ru>
2024-10-01 13:44:47 +0300

    Выполнить тестовое задание №2 и №3

--- Mindbox Intern test/obj/Debug/net6.0/MindboxInterntest.AssemblyInfo.cs
+++ Mindbox Intern test/obj/Debug/net6.0/MindboxInterntest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MindboxInterntest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+314693866dbb6dac63bbe11715f66544ae3b9aac")]
 [assembly: System.Reflection.AssemblyProductAttribute("MindboxInterntest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MindboxInterntest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestProject1/obj/Debug/net6.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net6.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+314693866dbb6dac63bbe11715f66544ae3b9aac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
